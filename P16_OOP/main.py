07635dd90a3281909ae08e2ff97a747f533a62f7
tejjm <134138033+tejjm@users.noreply.github.com>
2025-08-09 21:37:59 +0530

    Add files via upload

---
+++ P16_OOP/main.py
@@ -0,0 +1,26 @@
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+import time
+cf = CoffeeMaker()
+menu = Menu()
+money = MoneyMachine()
+user_input = ''
+while user_input != False:
+    user_input =input(f"Welcome to Teju Coffee, What would you like? \n{menu.get_items()}\n")
+    if user_input == "report":
+        cf.report()
+        money.report()
+    elif user_input == "off":
+        user_input = True
+    else:
+        order = menu.find_drink(user_input)
+        if cf.is_resource_sufficient(order):
+            if money.make_payment(order.cost):
+                cf.make_coffee(order)
+                time.sleep(2)
+                print("\n" * 100)
+
+
+
