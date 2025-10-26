f32243521c058c68a9e4d830a754c279532c93b5
tejjm <uttej.madhusudhan@gmail.com>
2025-10-26 15:21:42 +0000

    added authentication and updated empty IATA column with testing

--- Day 39 Flight Deal Finder/data_manager.py
+++ Day 39 Flight Deal Finder/data_manager.py
@@ -1,3 +1,41 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/f332308f593d58afb67838a2cba10df6/flightDealsUttej/prices"
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self._user = os.environ["SHEETY_USERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data,
+                auth=self._authorization
+            )
+            print(response.text)
--- Day 39 Flight Deal Finder/flight_data.py
+++ Day 39 Flight Deal Finder/flight_data.py
@@ -1,3 +1,6 @@
-class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+class FlightSearch:
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
