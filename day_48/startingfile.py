7dd059eb3cecfef139e8b3afb28d5769a3d9272b
tejjm <uttej.madhusudhan@gmail.com>
2025-12-16 15:29:25 +0000

    Tried the same function as last time but with dict comprehension

--- day_48/startingfile.py
+++ day_48/startingfile.py
@@ -8,19 +8,21 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 # price = driver.find_element(By.XPATH,value='//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[3]/span[2]/span[2]').text
 # print(f"The shirts price today is {price}")
-# event_times = driver.find_elements(By.CSS_SELECTOR,"div.medium-widget.event-widget.last ul.menu li time")
-# events = driver.find_elements(By.CSS_SELECTOR,"div.medium-widget.event-widget.last ul.menu li a")
-events = driver.find_elements(By.CSS_SELECTOR,"div.medium-widget.event-widget.last ul.menu li")
+event_times = driver.find_elements(By.CSS_SELECTOR,"div.medium-widget.event-widget.last ul.menu li time")
+events = driver.find_elements(By.CSS_SELECTOR,"div.medium-widget.event-widget.last ul.menu li a")
+# events = driver.find_elements(By.CSS_SELECTOR,"div.medium-widget.event-widget.last ul.menu li")
 # for event in event_times:
 #     print(event.text)
-# time = [time.text for time in event_times]
-# events = [event.text for event in events]
-event_details = {}
-event_list = []
-for event in events:
-    split =event.text.split("\n")
-    event_details["time"] = split[0]
-    event_details["name"] = split[1]
-    event_list.append(event_details)
-pprint(event_list)
+time = [time.text for time in event_times]
+events = [event.text for event in events]
+# event_details = {}
+# event_list = []
+# for event in events:
+#     split =event.text.split("\n")
+#     event_details["time"] = split[0]
+#     event_details["name"] = split[1]
+#     event_list.append(event_details)
+# pprint(event_list)
+event_details = {n:{"time":time[n],"name":events[n]} for n in range(len(time))}
+pprint(event_details)
 driver.quit()
