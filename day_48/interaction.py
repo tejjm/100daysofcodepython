4dc91754446561a15f420ee6a21dde492c00f064
tejjm <uttej.madhusudhan@gmail.com>
2025-12-17 16:03:02 +0000

    interacting with website, clicking and keys

--- day_48/interaction.py
+++ day_48/interaction.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-URL = "https://en.wikipedia.org/wiki/Main_Page"
+from selenium.webdriver.common.keys import Keys
+URL = "https://secure-retreat-92358.herokuapp.com/"
+FIRST_NAME = "Uttej"
+LAST_NAME = "Bankai"
+EMAIL = "bankaibankai@bankai.com"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
-articles = driver.find_element(By.XPATH,'//*[@id="articlecount"]/ul/li[2]/a[1]').text
-print(f"Wikipedia has {articles} articles currently")
-driver.quit()
+fname = driver.find_element(By.XPATH,"/html/body/form/input[1]")
+lname = driver.find_element(By.XPATH,"/html/body/form/input[2]")
+email_ = driver.find_element(By.XPATH,"/html/body/form/input[3]")
+fname.send_keys(FIRST_NAME,Keys.ENTER)
+lname.send_keys(LAST_NAME,Keys.ENTER)
+email_.send_keys(EMAIL,Keys.ENTER)
+sign_up = driver.find_element(By.XPATH,"/html/body/form/button")
+sign_up.click()
+
+# driver.quit()
