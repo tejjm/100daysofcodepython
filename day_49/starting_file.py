1eb26f6dbd4446c1b3eb33d8c35681055d71b421
tejjm <uttej.madhusudhan@gmail.com>
2025-12-22 16:34:41 +0000

    Created profile and dummy values for first time

--- day_49/starting_file.py
+++ day_49/starting_file.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
-#Accessing the web page
+import os
+ACCOUNT_EMAIL = "uttejgym@gmail.com"#Dummy values which work for this session
+ACCOUNT_PASSWORD = "Uttejgym@1996"
 URL = "https://appbrewery.github.io/gym/"
+#Accessing the web page
 chrome_options = webdriver.ChromeOptions()
+#Creating chrome profile and storing profile
+user_data_dir = os.path.join(os.getcwd(),"chrome_profile")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 chrome_options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
