c9ecb11a66247befe5ccbfd804f0d10f1ff0ba20
tejjm <uttej.madhusudhan@gmail.com>
2025-12-23 15:51:17 +0000

    Automated login finished

--- day_49/starting_file.py
+++ day_49/starting_file.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
@@ -18,3 +19,9 @@ driver.get(URL)
 #Automating login
 login = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="login-button"]')))
 login.click()
+enter_email = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="email-input"]')))
+enter_email.send_keys(ACCOUNT_EMAIL)
+enter_pw = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="password-input"]')))
+enter_pw.send_keys(ACCOUNT_PASSWORD)
+login_after_credentials = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="submit-button"]')))
+login_after_credentials.click()
