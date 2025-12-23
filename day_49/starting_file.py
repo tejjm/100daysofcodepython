b0ecdade5453ce048616718afa4fece5a780a0fd
tejjm <uttej.madhusudhan@gmail.com>
2025-12-23 15:41:43 +0000

    Automating login

--- day_49/starting_file.py
+++ day_49/starting_file.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 ACCOUNT_EMAIL = "uttejgym@gmail.com"#Dummy values which work for this session
 ACCOUNT_PASSWORD = "Uttejgym@1996"
@@ -12,3 +15,6 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 chrome_options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
+#Automating login
+login = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="login-button"]')))
+login.click()
