005cd6fd287e94d2a531ea98c3e6c64a29a63c7f
tejjm <uttej.madhusudhan@gmail.com>
2025-12-28 15:50:19 +0000

    Working code till finding the day elements needed

--- day_49/starting_file.py
+++ day_49/starting_file.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
+from pprint import pprint
 ACCOUNT_EMAIL = "uttejgym@gmail.com"#Dummy values which work for this session
-ACCOUNT_PASSWORD = "Uttejgym@1996"
+ACCOUNT_PASSWORD = "123123"
 URL = "https://appbrewery.github.io/gym/"
 #Accessing the web page
 chrome_options = webdriver.ChromeOptions()
@@ -26,10 +27,14 @@ enter_pw.send_keys(ACCOUNT_PASSWORD)
 login_after_credentials = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="submit-button"]')))
 login_after_credentials.click()
 #Grabbing the days from schedule page
-days = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'#schedule-page .Schedule_dayTitle__YBybs')))
-days_id = [day.get_attribute("id") for day in days]
-booking_day = input("Enter the day you want to book\n")
-for booking in days_id:
-    if booking_day[:3].lower() in booking:
-        id = booking
-print(id)
+booking_info = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'#schedule-page .Schedule_dayGroup__y79__ .Schedule_dayTitle__YBybs')))
+booking_text = [booking.text for booking in booking_info]
+count = 0
+booking_day = "Tuesday"
+for booking in booking_text:
+    count +=1
+    if booking_day[:3] in booking:
+        index = count-1
+        text_check = booking
+print(index)
+print(text_check)
