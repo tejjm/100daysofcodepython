df56820cc7f1f9f4c70c8a743a53c892485ba9ec
tejjm <uttej.madhusudhan@gmail.com>
2026-01-17 10:23:28 +0000

    login to bluesky successfull

--- day_51/starting_file.py
+++ day_51/starting_file.py
@@ -23,16 +23,25 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 # chrome_options.add_experimental_option('detach',True)
 driver = uc.Chrome(options=chrome_options,use_subprocess=True)
 driver.get(url=URL)
-time.sleep(random.uniform(1.5,4))
-# email = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input')))
-# for character in EMAIL:
-#     email.send_keys(character)
-#     time.sleep(random.uniform(0.05,0.18))
-# time.sleep(random.uniform(0.8,2.5))
+time.sleep(random.uniform(1.5,2.8))
+signin = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div[2]/div/div/div[2]/div[3]/div[2]/div/button/span')))
+signin.click()
+time.sleep(random.uniform(1.5,2.8))
+email = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[1]/input')))
+for character in EMAIL:
+    email.send_keys(character)
+    time.sleep(random.uniform(0.05,0.18))
+time.sleep(random.uniform(0.8,2.5))
 
-# next_button = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]/div/div/span/span')))
-# next_button.click()
-# time.sleep(random.uniform(1.5, 3.5))
+password = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/input')))
+for character in PW:
+    password.send_keys(character)
+    time.sleep(random.uniform(0.05,0.18))
+time.sleep(random.uniform(0.8,2.5))
+
+next_button = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[3]/button[2]')))
+next_button.click()
+time.sleep(random.uniform(1.5, 3.5))
 
 
 
