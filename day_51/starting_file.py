f7c155418fcb536826aa3b208f81641f6e9bd0fc
tejjm <uttej.madhusudhan@gmail.com>
2026-01-26 14:55:37 +0000

    started working on speed collector

--- day_51/starting_file.py
+++ day_51/starting_file.py
@@ -3,6 +3,8 @@ PROMISED_UPLOAD = 45
 EMAIL = 'johnbeenrecruiting@gmail.com'
 PW = 'Testing@1990'
 URL = 'https://bsky.app/'
+SPEED_URL = "https://www.speedtest.net/"
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -22,26 +24,32 @@ chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 # chrome_options.add_experimental_option('detach',True)
 driver = uc.Chrome(options=chrome_options,use_subprocess=True)
-driver.get(url=URL)
-time.sleep(random.uniform(1.5,2.8))
-signin = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div[2]/div/div/div[2]/div[3]/div[2]/div/button/span')))
-signin.click()
-time.sleep(random.uniform(1.5,2.8))
-email = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[1]/input')))
-for character in EMAIL:
-    email.send_keys(character)
-    time.sleep(random.uniform(0.05,0.18))
-time.sleep(random.uniform(0.8,2.5))
+driver.get(url=SPEED_URL)
+# speedtest_privacy = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="onetrust-accept-btn-handler"]')))
+# speedtest_privacy.click()
+# time.wait(2)
+# sppedtest_go = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'div[class*="start-button"]')))
+speedtest_go = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[2]/a/span[4]')))
+# speedtest_go.click()
+# time.sleep(random.uniform(1.5,2.8))
+# signin = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div[2]/div/div/div[2]/div[3]/div[2]/div/button/span')))
+# signin.click()
+# time.sleep(random.uniform(1.5,2.8))
+# email = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[1]/input')))
+# for character in EMAIL:
+#     email.send_keys(character)
+#     time.sleep(random.uniform(0.05,0.18))
+# time.sleep(random.uniform(0.8,2.5))
 
-password = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/input')))
-for character in PW:
-    password.send_keys(character)
-    time.sleep(random.uniform(0.05,0.18))
-time.sleep(random.uniform(0.8,2.5))
+# password = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/input')))
+# for character in PW:
+#     password.send_keys(character)
+#     time.sleep(random.uniform(0.05,0.18))
+# time.sleep(random.uniform(0.8,2.5))
 
-next_button = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[3]/button[2]')))
-next_button.click()
-time.sleep(random.uniform(1.5, 3.5))
+# next_button = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[3]/button[2]')))
+# next_button.click()
+# time.sleep(random.uniform(1.5, 3.5))
 
 
 
