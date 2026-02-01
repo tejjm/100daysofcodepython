0289c0184247d7a980a85292c784e7534a76fc88
tejjm <uttej.madhusudhan@gmail.com>
2026-02-01 14:05:24 +0000

    Login code done

--- day_52/starting_file.py
+++ day_52/starting_file.py
@@ -1,5 +1,5 @@
 EMAIL = 'johnbeenrecruiting@gmail.com'
-PW = 'JohnBeenJohn1996'
+PW = 'JohnBeenJohn@1996'
 INSTA_URL = "https://www.instagram.com/"
 import os
 from selenium.webdriver.common.by import By
@@ -8,6 +8,17 @@ from selenium.webdriver.support import expected_conditions as EC
 import undetected_chromedriver as uc
 import time
 import random
+
+def random_typer(func,text):
+    for letter in text:
+        time.sleep(random.uniform(0.10,0.15))
+        func(letter)
+def do_after_sec(func,sec):
+    time.sleep(sec)
+    try:
+        func()
+    except Exception as e:
+        print(e)
 
 class insta_follower_bot:
     def __init__(self):
@@ -24,6 +35,14 @@ class insta_follower_bot:
         
     def login(self):
         self.driver.get(INSTA_URL)
+        username = WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'input[name="email"]')))
+        random_typer(username.send_keys,EMAIL)
+        password = WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'input[name="pass"]')))
+        random_typer(password.send_keys,PW)
+        login = WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'span[style="--x---base-line-clamp-line-height: calc(var(--primary-label-line-height) * 1em); --x-lineHeight: calc(var(--primary-label-line-height) * 1em);"]')))
+        login.click()
+        profile = WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'a[href="/johnbeenrecruiting/"][role="link"]')))
+        profile.click()
         print("Press Enter to closer browser")
         input()
 
